eb8b27f37904425d2c05af19ba4f1df91586f474
Huper12345 <huper12345@gmail.com>
2022-11-27 21:41:01 +0300

    fix props

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,9 +5,9 @@ export const Footer = (props) => {
     return (
     <footer className="Footer">
       <div className="Footer__inner">
-           <div ElementData={props} className="Element1">Element1</div>
-           <div ElementData={props} className="Element2">Element2</div>
-           <div ElementData={props} className="Element3">Element3</div>
+           <div {...props} className="Element1">Element1</div>
+           <div {...props} className="Element2">Element2</div>
+           <div {...props} className="Element3">Element3</div>
       </div>
     </footer>
   );
