54ef4da1f2d689d08e031df1884366588a49bdb8
Kirill <115031997+Huper12345@users.noreply.github.com>
2022-11-29 13:07:33 +0300

    Update TrackInfo.js

--- src/components/Footer/TrackInfo/TrackInfo.js
+++ src/components/Footer/TrackInfo/TrackInfo.js
@@ -1,19 +1,19 @@
 import "./TrackInfo.scss";
 import { TrackInfoImage } from "./TrackInfoImage";
-import Summer from './Summer.png'
+import Summer from "./Summer.png";
 import { TrackInfoText } from "./TrackInfoText";
 import { TrackInfoLike } from "./TrackInfoLike";
 
 export const TrackInfo = (...props) => {
-    
-    return (
-    
-      <div className="TrackInfo">
-          <TrackInfoImage image={Summer} />
-          <TrackInfoText text={"Тест Текст"} name={"Тест Имя"} />
-          <TrackInfoLike />
-          
-      </div>
+  const likeData = (likeInfo) => {
+    console.log(likeInfo);
+  };
 
+  return (
+    <div className="TrackInfo">
+      <TrackInfoImage image={Summer} />
+      <TrackInfoText text={"Тест Текст"} name={"Тест Имя"} />
+      <TrackInfoLike func={likeData} />
+    </div>
   );
 };
