faadc2bf75e019a5ae08405545b6967956001f40
Kirill <115031997+Huper12345@users.noreply.github.com>
2022-11-29 13:05:25 +0300

    Update TrackInfoLike.js

--- src/components/Footer/TrackInfo/TrackInfoLike/TrackInfoLike.js
+++ src/components/Footer/TrackInfo/TrackInfoLike/TrackInfoLike.js
@@ -1,20 +1,25 @@
 import "./TrackInfoLike.css";
-import { LikeIcon } from './TrackInfoLikeIcon'
+import { LikeIcon } from "./TrackInfoLikeIcon";
 import { useState } from "react";
 
- export const TrackInfoLike = (props) => {
+export const TrackInfoLike = (props) => {
+  const [isActive, setActive] = useState(false);
 
-    const [isActive, setActive] = useState(false);
-
-    const toggleActive = () => {
-        setActive(!isActive);
+  const likeActive = () => {
+    setActive(!isActive);
+    if (!isActive) {
+      props.func("Liked");
+    } else {
+      props.func("Disliked");
     }
+  };
 
-    return (
-    
-      <button className={isActive ? 'TrackInfoLike active' : 'TrackInfoLike'} onClick={toggleActive}>
-        <LikeIcon />
-      </button>
-
+  return (
+    <button
+      className={isActive ? "TrackInfoLike active" : "TrackInfoLike"}
+      onClick={likeActive}
+    >
+      <LikeIcon />
+    </button>
   );
 };
