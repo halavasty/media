610744400f4722091b67c1c8bb43c7386429bf23
Huper12345 <huper12345@gmail.com>
2022-12-06 23:15:52 +0300

    add playbutton element

---
+++ src/components/Footer/MusicControl/MusicButtonTopbar/PlayControl/PlayButton/PlayButton.js
@@ -0,0 +1,8 @@
+import "./PlayButton.css";
+
+export const PlayButton = () => {
+  
+    return (
+      <button className="PlayButton">1</button>
+  );
+};
--- src/components/Footer/MusicControl/MusicButtonTopbar/PlayControl/PlayControl.js
+++ src/components/Footer/MusicControl/MusicButtonTopbar/PlayControl/PlayControl.js
@@ -1,12 +1,14 @@
 import "./PlayControl.css";
 import { LeftControlButton } from "./LeftControlButton";
 import { RightControlButton } from "./RightControlButton";
+import { PlayButton } from "./PlayButton";
 
 export const PlayControl = (...props) => {
     
     return (
       <div className="PlayControl">
         <LeftControlButton />
+        <PlayButton />
         <RightControlButton />
       </div>
   );
