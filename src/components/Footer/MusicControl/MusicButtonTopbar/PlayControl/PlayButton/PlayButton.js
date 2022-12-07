23a0480bb447271f28f25acb156688e2ce2a96b5
Huper12345 <huper12345@gmail.com>
2022-12-07 23:24:52 +0300

    add playButton component

--- src/components/Footer/MusicControl/MusicButtonTopbar/PlayControl/PlayButton/PlayButton.js
+++ src/components/Footer/MusicControl/MusicButtonTopbar/PlayControl/PlayButton/PlayButton.js
@@ -1,8 +1,23 @@
 import "./PlayButton.css";
+import { PlayButtonIcon } from "./PlayButtonIcon";
+import { useState } from "react";
 
 export const PlayButton = () => {
   
+    const [isActive, setActive] = useState(false);
+
+    const toggleActive = () => {
+        setActive(!isActive);
+        if (!isActive) {
+            console.log("Play");
+          } else {
+            console.log("Stop-play");
+          }
+    }
+    
     return (
-      <button className="PlayButton">1</button>
+      <button className={isActive ? 'PlayButton active' : 'PlayButton'} onClick={toggleActive}>
+        <PlayButtonIcon />
+      </button>
   );
 };
